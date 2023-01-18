81f46d4f246ca28814f9cab21a2bd2404c39a7ca
Rao Daniyal <raodaniyal1995@yahoo.com>
2023-01-18 08:31:42 +0500

    home page work

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   siteUrl: `${process.env.APP_URL}`,
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
 }
 
 module.exports = nextConfig
