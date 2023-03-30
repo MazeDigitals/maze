727140dc6f8fc0b2365f5ce7d3a6062177cdad8a
daniyalrajputt <raodaniyal1995@yahoo.com>
2023-03-30 19:03:19 +0500

    revert changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const { join } = require('path')
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   experimental: {
     outputStandalone: true,
-    outputFileTracingRoot: join(__dirname, '../../'),
-    esmExternals: false,
   }
 }
 
