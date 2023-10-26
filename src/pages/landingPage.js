a55beb01fb0dc613dbcf3d54bc181b7c0a0b9623
Rao Daniyal <raodaniyal1995@yahoo.com>
2023-10-26 18:09:24 +0500

    new landing page completed

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import { Inter } from "@next/font/google";
 import { Col, Container, Row } from "reactstrap";
 import styles from "@/styles/LandingPage.module.scss";
 import Link from "next/link";
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from 'reactstrap';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,18 @@ const DynamicFooter = dynamic(() => import("components/Footer/Footer"), {
 });
 
 const landingPage = () => {
+
+
+  const [open, setOpen] = useState('1');
+  const toggle = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };
+
+
   return (
     <>
       <Head>
@@ -141,6 +155,206 @@ const landingPage = () => {
           </Container>
         </section>
         {/* Services Section Ends */}
+
+        <section className={`${styles.services_section} ${styles.wp_services}`}>
+          <Container>
+            <Row className="justify-content-between">
+              <Col className="col-md-12 col-12 text-center mb-5">
+                <h2 className="fs-5 fw-semibold mb-3 opensans"><img src="/images/logo-icon.png" class="img-fluid" alt="Services" /> MAZE DIGITAL</h2>
+                <h3 className="fw-bold opensans">Dependable WordPress Services</h3>
+                <p className="fs-4 fw-normal">From the initial design phase to ongoing support, Maze Digital has you covered.</p>
+              </Col>
+
+              <Col className="col-md-5 col-12">
+                <div className={styles.wp_service_card}>
+                  <figure>
+                    <img src="/images/wp-1.png" class="img-fluid" alt="icon" />
+                  </figure>
+                  <div className={styles.content}>
+                    <h4 className="fw-semibold mb-0 opensans">WordPress design</h4>
+                    <p className="fs-6 fw-normal mb-0">Cutting-edge design techniques meet world-class WordPress solutions.</p>
+                  </div>
+                </div>
+                <div className={styles.wp_service_card}>
+                  <figure>
+                    <img src="/images/wp-2.png" class="img-fluid" alt="icon" />
+                  </figure>
+                  <div className={styles.content}>
+                    <h4 className="fw-semibold mb-0 opensans">WordPress development</h4>
+                    <p className="fs-6 fw-normal mb-0">Our developers bring your vision to life, ensuring a site that's both functional and aesthetically pleasing.</p>
+                  </div>
+                </div>
+                <div className={styles.wp_service_card}>
+                  <figure>
+                    <img src="/images/wp-3.png" class="img-fluid" alt="icon" />
+                  </figure>
+                  <div className={styles.content}>
+                    <h4 className="fw-semibold mb-0 opensans">WordPress support</h4>
+                    <p className="fs-6 fw-normal mb-0">Round-the-clock assistance ensures your site runs smoothly, always.</p>
+                  </div>
+                </div>
+                <div className={styles.wp_service_card}>
+                  <figure>
+                    <img src="/images/wp-4.png" class="img-fluid" alt="icon" />
+                  </figure>
+                  <div className={styles.content}>
+                    <h4 className="fw-semibold mb-0 opensans">WordPress maintenance</h4>
+                    <p className="fs-6 fw-normal mb-0">We handle the technicalities, ensuring optimal site performance.</p>
+                  </div>
+                </div>
+                <div className={styles.wp_service_card}>
+                  <figure>
+                    <img src="/images/wp-5.png" class="img-fluid" alt="icon" />
+                  </figure>
+                  <div className={styles.content}>
+                    <h4 className="fw-semibold mb-0 opensans">WordPress retainers</h4>
+                    <p className="fs-6 fw-normal mb-0">Benefit from a dedicated team that knows your business and needs.</p>
+                  </div>
+                </div>
+              </Col>
+              <Col className="col-md-5 col-12">
+                <figure className={styles.img_wrapper}>
+                  <img src="/images/star-1.svg" class={`img-fluid ${styles.star}`} alt="Star" />
+                  <img src="/images/3d-image.png" class="img-fluid" alt="3d rendering website hosting concept" />
+                </figure>
+              </Col>
+
+
+              <Col className="col-12 text-center mt-5">
+                <Link href="#" className={`${styles.btnstyle}`}>GET STARTED NOW <img src="/images/arrow-long.svg" class="img-fluid" alt="Star" /></Link>
+              </Col>
+            </Row>
+
+          </Container>
+        </section>
+
+
+        {/* Achivements Section */}
+        <section className={`${styles.achivement_section}`}>
+          <img src='images/achievment_bg.png' alt='bg' className={styles.bg_img} />
+          <Container>
+            <Row className='justify-content-center'>
+              <Col className='col-lg-4 col-md-8 col-12 text-center mb-4'>
+                <h4 className='fw-semibold text-white mb-2'>ACHIEVEMENTS</h4>
+                <p className='fw-normal text-white'>We are thrilled to put together some amazing projects that got us positive reviews from our loyal clients.</p>
+              </Col>
+            </Row>
+            <Row className='justify-content-center'>
+              <Col className='col-lg-4 col-md-6 col-12'>
+                <div className={`${styles.card} text-center`}>
+                  <h3 className='fs-5 fw-normal'>Maze Digital is rated <br /> Excellent</h3>
+                  <div className='d-flex align-items-center justify-content-center my-2'>
+                    <img src='/images/trustpilot-star.svg' alt="icon" />
+                    <img src='/images/trustpilot-star.svg' alt="icon" />
+                    <img src='/images/trustpilot-star.svg' alt="icon" />
+                    <img src='/images/trustpilot-star.svg' alt="icon" />
+                    <img src='/images/trustpilot-star.svg' alt="icon" />
+                  </div>
+                  <p className='fs-7 fw-light'>Based on <strong className='fw-semibold'>456 reviews</strong></p>
+                  <span className='fs-6 fw-normal d-flex'><img src='/images/trustpilot-logo.svg' alt="icon" />Trustpilot</span>
+                </div>
+              </Col>
+              <Col className='col-lg-4 col-md-6 col-12'>
+                <div className={`${styles.card} text-center`}>
+                  <h3 className='fs-5 fw-normal'>Maze Digital is rated with <br /> 4.8 Stars</h3>
+                  <div className='d-flex align-items-center justify-content-center my-2'>
+                    <img src='/images/star.svg' alt="icon" />
+                    <img src='/images/star.svg' alt="icon" />
+                    <img src='/images/star.svg' alt="icon" />
+                    <img src='/images/star.svg' alt="icon" />
+                    <img src='/images/star.svg' alt="icon" />
+                  </div>
+                  <p className='fs-7 fw-light'>Based on <strong className='fw-semibold'>152 reviews</strong></p>
+                  <span className='fs-6 fw-normal d-flex'><img src='/images/google.svg' alt="icon" />Google Reviews</span>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* Achivements Section */}
+
+        {/* Get In TOUCH */}
+        <section className={styles.get_in_touch}>
+          <Container>
+            <Row>
+              <Col className="col-12 text-center">
+                <ul>
+                  <li><img src='/images/avatar-1.png' alt="icon" /></li>
+                  <li><img src='/images/avatar-2.png' alt="icon" /></li>
+                  <li><img src='/images/avatar-3.png' alt="icon" /></li>
+                </ul>
+                <h3 className="fs-4 fw-medium">Still have questions?</h3>
+                <p className="fs-5 fw-normal">We're here to assist. Chat with our dedicated team today for all your WordPress needs.</p>
+                <Link href="#" className="btn-style text-uppercase">Get In Touch</Link>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* Get In TOUCH */}
+
+        <section className={styles.faq_section}>
+          <Container>
+            <Row className="justify-content-center">
+              <Col className="col-12 text-center mb-5">
+                <h2 className="fs-1 mb-2">Frequently asked questions</h2>
+                <p className="fs-4 fw-normal">Everything you need to know about the Maze Digital Services.</p>
+              </Col>
+
+              <Col className="col-md-8 col-12">
+                <Accordion open={open} toggle={toggle}>
+                  <AccordionItem>
+                    <AccordionHeader targetId="1">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">What is Maze Digital's primary expertise?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="1">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader targetId="2">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">How experienced is the Maze Digital team with WordPress?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="2">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader targetId="3">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">What sets Maze Digital apart in terms of service reliability?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="3">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader targetId="4">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">Do you offer continuous support and maintenance for WordPress websites?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="4">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader targetId="5">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">How can I initiate a project with Maze Digital?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="5">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionHeader targetId="6">
+                      <h3 className="fs-5 opensans fw-semibold mb-0">How does Maze Digital ensure client satisfaction?</h3>
+                    </AccordionHeader>
+                    <AccordionBody accordionId="6">
+                      <p>Maze Digital specializes in website design and development, with a focus on ensuring your website isn't just a brochure, but a tool that offers a great user experience and helps convert leads.</p>
+                    </AccordionBody>
+                  </AccordionItem>
+                </Accordion>
+              </Col>
+            </Row>
+          </Container>
+        </section>
 
         <DynamicFooter />
       </main>
