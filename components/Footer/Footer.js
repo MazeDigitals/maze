bf76b0404538f15a635a73b9333b4755a69e221a
MazeDigitals <malizeeshan52@gmail.com>
2024-01-24 01:18:10 +0500

    update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
                                     <UncontrolledCollapse toggler="#toggler-1" className={styles.collapse_wrap}>
                                         <p className='text-white'>7901, 4th st N STE 5217, St.Peterburg FL, USA 33702</p>
                                         <p className='text-white'>Unit 13 1940 Ironstone Drive, Burlington, ON, Canada L5A3L7</p>
-                                        <p className='text-white'>Office No. 14, Building @8, Dubai Media City, Dubai, AE</p>
+                                        <p className='text-white'>Office No. 14, Building #8, Dubai Media City, Dubai, AE</p>
                                     </UncontrolledCollapse>
                                 </div>
                                 <div className={styles.menu_wrap}>
@@ -44,7 +44,7 @@ const Footer = () => {
                                     <UncontrolledCollapse toggler="#toggler-2" className={styles.collapse_wrap}>
                                         <ul>
                                             <li><a href='mailto:sales@mazedigitals.com' className='fs-6 d-inline-flex'><FaRegEnvelope /> sales@mazedigitals.com <br />info@mazedigitals.com</a></li>
-                                            <li><a href='tel:+1 330-999-8028' className='fs-6'><FaPhoneAlt />US/CA : +1 330-999-8028</a></li>
+                                            <li><a href='tel:+1 330-999-8028' className='fs-6'><FaPhoneAlt />US\CA : +1 330-999-8028</a></li>
                                             <li><a href='tel:+917 50 117 0371' className='fs-6'><FaPhoneAlt />UAE : +917 50 117 0371</a></li>
                                         </ul>
                                     </UncontrolledCollapse>
@@ -54,7 +54,7 @@ const Footer = () => {
                                     <li><a href='#'><FaFacebookF /></a></li>
                                     <li><a href='#'><FaInstagram /></a></li>
                                 </ul>
-                                <p className='text-center text-white fs-7 mb-2'>© 2023 Maze Digital. All rights reserved </p>
+                                <p className='text-center text-white fs-7 mb-2'>© 2024 Maze Digital. All rights reserved </p>
                                 <p className='text-center text-white fs-7'>
                                     <a className='text-white' href='#'>Terms of Use</a> |
                                     <a className='text-white' href='#'>Privacy Policy</a> |
@@ -97,7 +97,7 @@ const Footer = () => {
                             <h3>Get in Touch</h3>
                             <ul>
                                 <li><a href='mailto:sales@mazedigitals.com' className='d-inline-flex'><FaRegEnvelope /> sales@mazedigitals.com <br />info@mazedigitals.com</a></li>
-                                <li><a href='tel:+1 330-999-8028'><FaPhoneAlt /> +1 330-999-8028</a></li>
+                                <li><a href='tel:+1 330-999-8028'><FaPhoneAlt />US\CA : +1 330-999-8028</a></li>
                                 <li><a href='tel:+917 50 117 0371'><FaPhoneAlt />UAE : +917 50 117 0371</a></li>
                             </ul>
                         </Col>
@@ -107,14 +107,14 @@ const Footer = () => {
                                 <li><address>7901, 4th st N STE 5217,
                                     St.Peterburg FL, USA 33702</address></li>
                                 <li><address>Unit 13 1940 Ironstone Drive, Burlington, ON, Canada L5A3L7</address></li>
-                                <li><address>Office No. 14, Building @8, Dubai Media City, Dubai, AE</address></li>
+                                <li><address>Office No. 14, Building #8, Dubai Media City, Dubai, UAE</address></li>
                             </ul>
                         </Col>
                         <Col className='col-12 mt-4'>
                             <ul className='d-flex align-items-center'>
                                 <li><a href='#' className='me-2'>Terms and Condition</a></li>
                                 <li><a href='#'>Privacy Policy</a></li>
-                                <li className='ms-auto'><p>© 2023 Maze Digital. All rights reserved </p></li>
+                                <li className='ms-auto'><p>© 2024 Maze Digital. All rights reserved </p></li>
                             </ul>
                         </Col>
                     </Row>
