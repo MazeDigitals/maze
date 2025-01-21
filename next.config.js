4b43ac78b4ddadaa8903e6996250e6362ce6ba7f
Rao Daniyal <raodaniyal1995@yahoo.com>
2025-01-21 22:18:39 +0500

    added ignore lint in next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // Updated from experimental.outputStandalone
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during builds
+  },
 };
 
 module.exports = nextConfig;
